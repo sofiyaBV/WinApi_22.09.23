5ae0bfafd526d26ac84a0010b56eba410d3c5f23
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-09-22 19:44:51 +0300

    калькулятор вместо блокнота

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,25 @@ namespace WinApi_22._09._23
         public MainWindow()
         {
             InitializeComponent();
+        }
+      
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName,   string lpWindowName);
+      
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private void Click_click(object sender, RoutedEventArgs e)
+        {
+            IntPtr notepadHandle = FindWindow(null, "Calculator");
+            if (notepadHandle  != IntPtr.Zero) {
+                SendMessage(notepadHandle, 0x0010, IntPtr.Zero, IntPtr.Zero);
+               MessageBox.Show("Вікно калькулятор закрито.");
+            }
+            else
+            {
+               MessageBox.Show("Вікно калькулятор не знайдено.");
+            }
         }
     }
 }
